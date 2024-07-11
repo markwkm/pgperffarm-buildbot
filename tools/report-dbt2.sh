6263ac6271afa4807a9ce829f33532bf813185f7
Mark Wong <markwkm@gmail.com>
2024-07-10 20:49:18 -0700

    Update reporting scripts for scale plus fixes
    Generate a set of charts for different scale factors.  Tests generally should
    be compared to the same scale factor.
    
    * Only plot the latest results for the same revision
    * If there is no "revision", use "got_revision"
    * Ignore results where we don't know the branch
    * Get scale factor details from test output from AnyBranchSchedule builds

--- tools/report-dbt2.sh
+++ tools/report-dbt2.sh
@@ -9,90 +9,145 @@ PLOTSIZE="1600,1000"
 
 psql -X -d perffarm -o "${CSVEXPORT}" << __SQL__
 COPY (
-    SELECT workers.name AS plant
-         , btrim(branch.value, '"') AS branch
-         , btrim(revision.value, '"') AS revision
-         , scale.value AS scale
-         , logs.id AS logs_id
-    FROM builders
-       , builds
-       , workers
-       , build_properties AS branch
-       , build_properties AS revision
-       , build_properties AS scale
-       , steps
-       , logs
-    WHERE builderid = builders.id
-      AND workerid = workers.id
-      AND steps.buildid = builds.id
-      AND stepid = steps.id
-      AND branch.buildid = builds.id
-      AND revision.buildid = builds.id
-      AND scale.buildid = builds.id
-      AND builders.name = 'dbt2'
-      AND builds.results = 0
-      AND branch.name = 'branch'
-      AND revision.name = 'revision'
-      AND scale.name = 'warehouses'
-      AND steps.name = 'DBT-2 Summary'
+    WITH data AS (
+        SELECT workers.name AS plant
+             , btrim(branch.value, '"') AS branch
+             , CASE WHEN revision.value = '""'
+                    THEN btrim(got_revision.value, '"')
+                    ELSE btrim(revision.value, '"')
+                    END AS revision
+             , scale.value AS scale
+             , log_summary.id AS log_summary_id
+             , log_test.id AS log_test_id
+             , row_number() OVER (
+                   PARTITION BY workers.name
+                              , branch.value
+                              , revision
+                              , scale.value
+                   ORDER BY builds.complete_at DESC
+               ) AS latest
+        FROM workers
+             JOIN builds
+               ON builds.workerid = workers.id
+              AND builds.results = 0
+             JOIN builders
+               ON builders.id = builderid
+             JOIN build_properties AS branch
+               ON branch.buildid = builds.id
+              AND branch.name = 'branch'
+              AND branch.value <> '""'
+             JOIN build_properties AS revision
+               ON revision.buildid = builds.id
+              AND revision.name = 'revision'
+             JOIN build_properties AS got_revision
+               ON got_revision.buildid = builds.id
+              AND got_revision.name = 'got_revision'
+             LEFT OUTER JOIN build_properties AS scale
+               ON scale.buildid = builds.id
+              AND scale.name = 'warehouses'
+             JOIN steps AS step_summary
+               ON step_summary.buildid = builds.id
+              AND step_summary.name = 'DBT-2 Summary'
+             JOIN logs AS log_summary
+               ON log_summary.stepid = step_summary.id
+             LEFT OUTER JOIN steps AS step_test
+               ON step_test.buildid = builds.id
+              AND step_test.name = 'Performance test'
+             LEFT OUTER JOIN logs AS log_test
+               ON log_test.stepid = step_test.id
+        WHERE builders.name = 'dbt2'
+          OR builders.name LIKE 'dbt2-%'
+    )
+    SELECT plant
+         , branch
+         , revision
+         , scale
+         , log_summary_id
+         , log_test_id
+    FROM data
+    WHERE latest = 1
 )
 TO STDOUT
-(FORMAT csv, HEADER TRUE, DELIMITER ' ');
+(FORMAT csv, HEADER TRUE, DELIMITER ' ', NULL 'NULL');
 __SQL__
 
-HEADER="$(head -n 1 "${CSVEXPORT}") ctime metric"
+HEADER="$(head -n 1 "${CSVEXPORT}") ctime metric warehouses"
+
+(cd "${PGGITDIR}" && git fetch -q --all && git pull -q)
 
 echo "${HEADER}" > "${CSVREPORT}"
 tail -n +2 "${CSVEXPORT}" | while IFS= read -r LINE; do
 	COMMIT="$(echo "${LINE}" | cut -d " " -f 3)"
-	LOGSID="$(echo "${LINE}" | cut -d " " -f 5)"
+	SCALE="$(echo "${LINE}" | cut -d " " -f 4)"
+	LOGSUMMARY="$(echo "${LINE}" | cut -d " " -f 5)"
+	LOGTEST="$(echo "${LINE}" | cut -d " " -f 6)"
 
 	CTIME="$(cd "${PGGITDIR}" && git show -s --format="%ct" "${COMMIT}")"
 
-	NOTPM="$(curl --silent "${BUILDBOTURL}/api/v2/logs/${LOGSID}/raw_inline" \
+	NOTPM="$(curl --silent "${BUILDBOTURL}/api/v2/logs/${LOGSUMMARY}/raw_inline" \
 			| sed -n 's/.*Throughput: \([0-9]\+\(\.[0-9]\+\)\?\).*/\1/p')"
 
-	echo "${LINE} ${CTIME} ${NOTPM}" >> "${CSVREPORT}"
+	if [ "${SCALE}" = "NULL" ]; then
+		WAREHOUSES="$(curl --silent "${BUILDBOTURL}/api/v2/logs/${LOGTEST}/raw_inline" \
+				| sed -n 's/.*SCALE FACTOR (WAREHOUSES): \([0-9]\+\(\.[0-9]\+\)\?\).*/\1/p')"
+	else
+		WAREHOUSES="${SCALE}"
+	fi
+
+	echo "${LINE} ${CTIME} ${NOTPM} ${WAREHOUSES}" >> "${CSVREPORT}"
 done
 
-tail -n +2 "${CSVREPORT}" | sort -t " " -k 6 -n > "${CSVSORTED}"
+tail -n +2 "${CSVREPORT}" | sort -t " " -k 7 -n > "${CSVSORTED}"
 
 BRANCHES="$(tail -n +2 "${CSVEXPORT}" | cut -d " " -f 2 | sort -u | xargs)"
 PLANTS="$(tail -n +2 "${CSVEXPORT}" | cut -d " " -f 1 | sort -u | xargs)"
+SCALES="$(tail -n +2 "${CSVSORTED}" | cut -d " " -f 9 | sort -u | xargs)"
 
-for PLANT in ${PLANTS}; do
-	COUNT=0
-	PLOTLIST=""
-	TMPFILELIST=""
-	for BRANCH in ${BRANCHES}; do
-		COUNT=$(( COUNT + 1 ))
-		TMPFILE="$(mktemp)"
-		TMPFILELIST="${TMPFILELIST} ${TMPFILE}"
+TMPFILELIST=""
+for SCALE in ${SCALES}; do
+	TMPFILESCALE="$(mktemp)"
+	TMPFILELIST="${TMPFILELIST} ${TMPFILESCALE}"
+	awk -F " " "\$9 == \"${SCALE}\"" "${CSVSORTED}" >> "${TMPFILESCALE}"
 
-		if [ ${COUNT} -gt 1 ]; then
-			PLOTLIST="${PLOTLIST},"
-		fi
-		PLOTLIST="${PLOTLIST}'${TMPFILE}' using 'ctime':'metric' title '${BRANCH}' noenhanced with linespoints"
+	for PLANT in ${PLANTS}; do
+		COUNT=0
+		PLOTLIST=""
+		for BRANCH in ${BRANCHES}; do
+			TMPFILE="$(mktemp)"
+			TMPFILELIST="${TMPFILELIST} ${TMPFILE}"
 
-		echo "${HEADER}" > "${TMPFILE}"
-		awk -F " " "\$2 == \"${BRANCH}\"" "${CSVSORTED}" >> "${TMPFILE}"
+			echo "${HEADER}" > "${TMPFILE}"
+			awk -F " " "\$1 == \"${PLANT}\" && \$2 == \"${BRANCH}\"" \
+					"${TMPFILESCALE}" >> "${TMPFILE}"
+
+			LINES="$(wc -l "${TMPFILE}" | cut -d " " -f 1)"
+			if [ "${LINES}" -eq 1 ]; then
+				continue
+			fi
+
+			COUNT=$(( COUNT + 1 ))
+			if [ ${COUNT} -gt 1 ]; then
+				PLOTLIST="${PLOTLIST},"
+			fi
+			PLOTLIST="${PLOTLIST}'${TMPFILE}' using 'ctime':'metric' title '${BRANCH}' noenhanced with linespoints"
+		done
+
+		gnuplot <<- __PLOT__
+			set xdata time
+			set timefmt "%s"
+			set terminal pngcairo size ${PLOTSIZE}
+			set xlabel "Time"
+			set xtics rotate
+			set xtics format "%Y-%m-%d"
+			set grid
+			set title "DBT-2 Results ${PLANT} ${SCALE} Warehouses" noenhanced
+			set output 'dbt2-${PLANT}-${SCALE}.png'
+			set ylabel "New Orders / Minute"
+			set yrange [0:*]
+			set key below
+			plot ${PLOTLIST}
+		__PLOT__
 	done
-
-	gnuplot <<- __PLOT__
-		set xdata time
-		set timefmt "%s"
-		set terminal pngcairo size ${PLOTSIZE}
-		set xlabel "Time"
-		set xtics rotate
-		set xtics format "%Y-%m-%d"
-		set grid
-		set title "DBT-2 Results ${PLANT}" noenhanced
-		set output 'dbt2-${PLANT}.png'
-		set ylabel "New Orders / Minute"
-		set yrange [0:*]
-		set key below
-		plot ${PLOTLIST}
-	__PLOT__
-
-	rm "${TMPFILELIST}"
 done
+
+echo "${TMPFILELIST}" | xargs rm
